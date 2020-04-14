4a5217d1cb3ee815eb243e1ec9bbe37759a5a512
Ben Swierzy <swierzy@uni-bonn.de>
2020-04-14 12:58:49 +0200

    Initial Hello World setup

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ *  systemboard
+ *  Copyright (C) 2020 Ben Swierzy
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+declare(strict_types=1);
+
+use Slim\Factory\AppFactory;
+
+require 'handler.php';
+require 'vendor/autoload.php';
+require 'src/autoload.php';
+
+$services = require 'services.php';
+
+AppFactory::setContainer($services);
+$app = AppFactory::create();
+
+$app->addBodyParsingMiddleware();
+$app->addRoutingMiddleware();
+$app->addErrorMiddleware(true, true, true);
+
+$app->get('/hello', $getHelloHandler);
+
+$app->run();
